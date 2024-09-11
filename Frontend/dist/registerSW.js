4c09b8c2bd6d55d80a87ccb1c1bca0140016d22e
Mike van Bree <mike.vanbree@student.pxl.be>
2024-09-11 21:55:03 +0200

    Fixed resource pathing by adding base parameter to Vite

--- Frontend/dist/registerSW.js
+++ Frontend/dist/registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/TileStorage/sw.js', { scope: '/TileStorage/' })})}
