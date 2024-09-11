4bbb87aa879b48f0e049949eb9eca6d0a43df83b
Mike van Bree <mike.vanbree@student.pxl.be>
2024-09-11 22:37:22 +0200

    Finalized About page

--- Frontend/src/components/Animated.vue
+++ Frontend/src/components/Animated.vue
@@ -1,21 +1,23 @@
 <template>
-    <h3 class="antialiased text-2xl">This website is made using:</h3>
-    <div>
-        <li class="animate__animated slideInLeft animation_1 hide-bullet-point">Vue 3</li>
-        <li class="animate__animated slideInLeft animation_2 hide-bullet-point">Vue Router</li>
-        <li class="animate__animated slideInLeft animation_3 hide-bullet-point">Vite</li>
-        <li class="animate__animated slideInLeft animation_4 hide-bullet-point">Vite PWA</li>
-        <li class="animate__animated slideInLeft animation_5 hide-bullet-point">Pinia</li>
-        <li class="animate__animated slideInLeft animation_6 hide-bullet-point">Tailwind CSS</li>
-        <li class="animate__animated slideInLeft animation_7 hide-bullet-point">Vuetify</li>
-        <li class="animate__animated slideInLeft animation_8 hide-bullet-point">Animate.css</li>
+    <div class="flex justify-center align-center">
+        <h4 class="text-center">This website is made using:</h4>
+        <ul>
+            <li class="animate__animated slideInLeft animation_1 button_style">Vue 3</li>
+            <li class="animate__animated slideInLeft animation_2 button_style">Vue Router</li>
+            <li class="animate__animated slideInLeft animation_3 button_style">Vite</li>
+            <li class="animate__animated slideInLeft animation_4 button_style">Vite PWA</li>
+            <li class="animate__animated slideInLeft animation_5 button_style">Pinia</li>
+            <li class="animate__animated slideInLeft animation_6 button_style">Tailwind CSS</li>
+            <li class="animate__animated slideInLeft animation_7 button_style">Vuetify</li>
+            <li class="animate__animated slideInLeft animation_8 button_style">Animate.css</li>
+        </ul>
     </div>
 </template>
 
 <style scoped>
 @keyframes slideInLeft {
   from {
-    transform: translate3d(-60vw, 0, 0);
+    transform: translate3d(-100vw, 0, 0);
     visibility: visible;
   }
 
@@ -27,63 +29,72 @@
   animation-name: slideInLeft;
 }
 
-.hide-bullet-point {
+ul {
     list-style-type: none;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
 }
+
+.button_style {
+    font-weight: bold;
+    padding: 0.25rem 0.75rem;
+    border-radius: 1rem;
+    margin-block: 0.125rem;
+}
+
 .animation_1 {
     animation-delay: 0.25s;
     animation-duration: 1.5s;
-    color: #41b883;
+    background-color: #42b883;
+    color: #35495e;
 }
 .animation_2 {
     animation-delay: 0.75s;
     animation-duration: 1.5s;
-    color: #496684;
+    background-color: #496684;
+    color: #42b883;
 }
 .animation_3 {
     animation-delay: 1.25s;
     animation-duration: 1.5s;
 
     /* Text gradiented */
-    background: #5AB4FF;
+    color: #f7d94b;
     background: linear-gradient(135deg, #5AB4FF 49%, #BD34FE 52%);
-    background-clip: text;
-    -webkit-background-clip: text;
-    -webkit-text-fill-color: transparent;
 }
 .animation_4 {
     animation-delay: 1.75s;
     animation-duration: 1.5s;
     
     /* Text gradiented */
-    background: linear-gradient(90deg, rgba(238,238,238,1) 45%, rgba(74,166,192,1) 46%, rgba(74,166,192,1) 53%, rgba(247,217,75,1) 54%);
-    background-clip: text;
-    -webkit-background-clip: text;
-    -webkit-text-fill-color: transparent;
+    color: #496684;
+    background: linear-gradient(135deg, #eeeeee 31%, #4aa6c0 34%, #4aa6c0 64%, #f7d94b 67%);
 }
 .animation_5 {
     animation-delay: 2.25s;
     animation-duration: 1.5s;
     color: #ffd34f;
+    background-color: #51b65c;
 }
 .animation_6 {
     animation-delay: 2.75s;
     animation-duration: 1.5s;
-    color: #38bdf8;
+    background: linear-gradient(135deg, #38bdf8 34%, #00000000 37%, #00000000 42%, #38bdf8 45%, #38bdf8 55%, #00000000 58%, #00000000 63%, #38bdf8 66%);
+    color: #eee;
 }
 .animation_7 {
     animation-delay: 3.25s;
     animation-duration: 1.5s;
-    color: #1ad1a5;
+    background-color: #aeddff;
+    color: #1697f6;
 }
 .animation_8 {
     animation-delay: 3.75s;
     animation-duration: 1.5s;
 
     /* Text gradiented */
-    background: linear-gradient(125deg, #623ac8 48%, #e69138 52%);
-    background-clip: text;
-    -webkit-background-clip: text;
-    -webkit-text-fill-color: transparent;
+    background: linear-gradient(125deg, #623ac8 48%, #a26524 52%);
+    color: #ffd3a5;
 }
 </style>
