3456d852b87ca1bfb7627652098c5f46378824da
Mike van Bree <mike.vanbree@student.pxl.be>
2024-09-13 21:38:46 +0200

    Updated tile cards for visual spacing

--- Frontend/src/components/CustomTileCard.vue
+++ Frontend/src/components/CustomTileCard.vue
@@ -13,22 +13,18 @@ export default {
 </script>
 
 <template>
-    <v-card width="90vw" height="100%" class="py-2 d-flex flex-row">
-        <div class="flex-col titles">
-            <v-card-title primary-title class="name my-0 py-0 pr-0">
-                {{ name }}
-            </v-card-title>
-            <v-card-title secordary-title class="size my-0 py-0">
-                {{ tileWidth }}x{{ tileLength }}
-            </v-card-title>
-        </div>
-        <v-card-text class="text-center">
-            <v-row>
-                <v-col height="100" cols="4"><span v-if="amountOfBoxes != null">{{  amountOfBoxes }}x</span></v-col>
-                <v-col cols="4"><span v-if="squareMetersPerBox != null">{{ squareMetersPerBox }}m<sup>2</sup></span></v-col>
-                <v-col cols="4"><span v-if="totalSquareMeters != null">{{ totalSquareMeters }}m<sup>2</sup></span></v-col>
-            </v-row>
-        </v-card-text> 
+    <v-card width="90vw" height="100%" class="py-0 d-flex flex-column">
+        <v-card-title class="pb-0 pt-2" primary-title>
+            {{ name }}
+        </v-card-title>
+        <v-card-text class="text-center card-text pb-1">
+            <div class="d-flex flex-row justify-space-between align-center">
+                <v-col class="px-0 justify-start" cols="y"><p>{{ tileWidth }}x{{ tileLength }}</p></v-col>
+                <v-col class="px-0 text-end" cols="3"><span v-if="amountOfBoxes != null">{{  amountOfBoxes }}x</span></v-col>
+                <v-col class="px-0" cols="3"><span v-if="squareMetersPerBox != null">{{ squareMetersPerBox }}m<sup>2</sup></span></v-col>
+                <v-col class="px-0" cols="3"><span v-if="totalSquareMeters != null">{{ totalSquareMeters }}m<sup>2</sup></span></v-col>
+            </div>
+        </v-card-text>
     </v-card>
 </template>
 
@@ -37,22 +33,7 @@ p {
     font-family: 'Montserrat', sans-serif;
     font-size: 150%;
 }
-
-.wrapper {
-    width: fit-content;
-    position: relative;
-    max-width: 100%; /* Ensures wrapping within the grid system */
-    line-height: 1.175;
-    
-}
-.name {
-    display: block;
-}
-.titles {
-    width: 50%;
-}
-.size {
-    font-family: 'montserrat', sans-serif;
-    font-weight: 300;
+.card-text {
+    margin-top: -0.8em;
 }
 </style>
--- Frontend/src/views/HomeView.vue
+++ Frontend/src/views/HomeView.vue
@@ -44,7 +44,7 @@ export default {
             <NewTileCard :to="{ name: 'TileDetails', params: { tileId: 'new' }}" />
             <div v-for="tile in this.tiles" :key="tile.id">
                 <RouterLink :to="{ name: 'TileDetails', params: { tileId: tile.id }}">
-                    <CustomTileCard height="100%" :name="tile.tilename" :tileWidth="tile.width" :tileLength="tile.length" :squareMetersPerBox="tile.squareMetersPerBox" :amountPerBox="tile.amountPerBox" :totalSquareMeters="tile.totalSquareMeters" />
+                    <CustomTileCard height="100%" :name="tile.tilename" :tileWidth="tile.width" :tileLength="tile.length" :amountOfBoxes="tile.amountOfBoxes" :squareMetersPerBox="((Math.round(100 * tile.squareMetersPerBox)) / 100.0)" :totalSquareMeters="tile.totalSquareMeters.toFixed(2)" />
                 </RouterLink>
             </div>
         </div>
