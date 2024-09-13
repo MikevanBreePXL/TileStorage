53bf3d8b4d3c0814866d0c61d7c9b1325067282f
Mike van Bree <mike.vanbree@student.pxl.be>
2024-09-13 15:03:53 +0200

    Created link for page to tile details

--- Frontend/src/router.ts
+++ Frontend/src/router.ts
@@ -3,11 +3,13 @@ import { createRouter, createWebHashHistory } from 'vue-router'
 import AboutView from './views/AboutView.vue'
 import EntryView from './views/EntryView.vue'
 import HomeView from './views/HomeView.vue'
+import TileDetails from './views/TileDetails.vue'
 
 const routes = [
   { path: '/', component: EntryView },
   { path: '/home', component: HomeView },
   { path: '/about', component: AboutView },
+  { path: '/tileDetails', component: TileDetails },
 ]
 
 const router = createRouter({
---
+++ Frontend/src/views/TileDetails.vue
@@ -0,0 +1,41 @@
+<script>
+import TopLogoBar from '../components/TopLogoBar.vue';
+
+export default {
+  name: 'TileDetails',
+  props: {
+    tileId: Number,
+    tileName: String,
+    tileLength: Number,
+    tileWidth: Number,
+    squareMetersPerBox: Number,
+    amountOfBoxes: Number,
+    totalSquareMeters: Number,
+  },
+  data() {
+    return {
+      tile: {
+        id: this.tileId,
+        name: this.tileName,
+        length: this.tileLength,
+        width: this.tileWidth,
+        squareMetersPerBox: this.squareMetersPerBox,
+        amountOfBoxes: this.amountOfBoxes,
+        totalSquareMeters: this.totalSquareMeters,
+      }
+    };
+  },
+  components: {
+    TopLogoBar,
+  },
+  methods: {
+    fillFields() {
+        // TODO: Fill fields with data from backend
+    }
+  }
+};
+</script>
+
+<template>
+    <TopLogoBar />
+</template>
