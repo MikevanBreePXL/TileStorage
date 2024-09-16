cd9e555953b54b0c24f3e3fc6c43e605a76b3c25
Mike van Bree <mike.vanbree@student.pxl.be>
2024-09-16 19:44:16 +0200

    detail changes

--- Frontend/src/components/HomeView/TileInfoCard.vue
+++ Frontend/src/components/HomeView/TileInfoCard.vue
@@ -1,16 +1,16 @@
 <template>
     <v-card width="90%">
         <v-card-text class="py-1">
-            <v-row>
+            <v-row class="align-center">
                 <v-col class="px-0 py-4 text-center" cols="3">
                     <font-awesome-layers class="fa-2x fa-fw">
                         <font-awesome-icon icon="fa-regular fa-circle inline-block" color="#073077" size="lg" />
                         <font-awesome-icon icon="fa-solid fa-info inline-block" color="#073077" size="sm" />
                     </font-awesome-layers>
                 </v-col>
-                <v-col class="px-0 text-end line-clamp-2" cols="3"><span>Aantal dozen</span></v-col>
-                <v-col class="px-0 text-center line-clamp-2" cols="3"><span>m<sup>2</sup> per doos</span></v-col>
-                <v-col class="px-2 text-center line-clamp-2" cols="3"><span>totaal m<sup>2</sup></span></v-col>
+                <v-col class="px-2 text-end line-clamp-2" cols="3"><span>Aantal dozen</span></v-col>
+                <v-col class="px-2 text-center line-clamp-2" cols="3"><span>m<sup>2</sup> per doos</span></v-col>
+                <v-col class="px-2 text-center line-clamp-2 align-center" cols="3"><span>totaal m<sup>2</sup></span></v-col>
             </v-row>
         </v-card-text>
     </v-card>
--- Frontend/src/views/TileDetails.vue
+++ Frontend/src/views/TileDetails.vue
@@ -36,6 +36,8 @@ export default {
   },
   methods: {
     saveTile() {
+      let form = document.getElementById('details-form');
+      form.classList.add('fadeOutRight');
       this.store.saveTile(this.tile);
 
       setTimeout(() => {
@@ -53,68 +55,71 @@ export default {
 </script>
 
 <template>
-  <div class="details-page d-flex flex-column">
+  <div id="details-page" class="details-page d-flex flex-column">
     <TopLogoBar />
+    <div id="details-form" class="animate__animated">
+      <div class="w-90 mx-auto mt-10 d-flex flex-column justify-center align-center">
+        <v-text-field
+          width="100%"
+          name="tileName"
+          label="Tegelnaam"
+          v-model="this.tile.tilename"
+        ></v-text-field>
 
-    <div class="w-90 mx-auto mt-10 d-flex flex-column justify-center align-center">
-      <v-text-field
-        width="100%"
-        name="tileName"
-        label="Tegelnaam"
-        v-model="this.tile.tilename"
-      ></v-text-field>
-
-      <div class="tile-size d-flex flex-row justify-center align-center w-100">
-        <v-text-field
-          name="tileWidth"
-          label="Breedte"
-          type="number"
-          v-model="this.tile.width"
-        ></v-text-field>
-        <span class="mx-2">x</span>
-        <v-text-field
-          name="tileLength"
-          label="Lengte"
-          type="number"
-          v-model="this.tile.length"
-        ></v-text-field>
-      </div>
-    
-      <div class="boxes d-flex flex-row align-center w-100">
-        <v-text-field
-          width="50%"
-          name="amountOfBoxes"
-          label="Aantal dozen"
-          v-model="this.tile.amountOfBoxes"
-        ></v-text-field>
-        <span class="mx-2">x</span>
-        <v-text-field
-          width="50%"
-          name="squareMetersPerBox"
-          label="m² per doos"
-          type="number"
-          v-model="this.tile.squareMetersPerBox"
-        ></v-text-field>
-      </div>
-
-      <div class="total-square-meters d-flex flex-row align-start w-100">
-        <div class="d-flex flex-column text-center justify-center align-center mr-5">
-          <v-checkbox-btn v-model="isTotalCalculated" @change="CalculateTotalSquareMeters()" hide-details></v-checkbox-btn>
-          <span style="color: #bbb;">Berekend</span>
+        <div class="tile-size d-flex flex-row justify-center align-center w-100">
+          <v-text-field
+            name="tileWidth"
+            label="Breedte"
+            type="number"
+            v-model="this.tile.width"
+          ></v-text-field>
+          <span class="mx-2">x</span>
+          <v-text-field
+            name="tileLength"
+            label="Lengte"
+            type="number"
+            v-model="this.tile.length"
+          ></v-text-field>
         </div>
-        <v-text-field
-          width="50%"
-          name="totalSquareMeters"
-          label="Totaal m²"
-          v-bind:disabled="isTotalCalculated"
-          v-model="this.tile.totalSquareMeters"
-        ></v-text-field>
+      
+        <div class="boxes d-flex flex-row align-center w-100">
+          <v-text-field
+            width="50%"
+            name="amountOfBoxes"
+            label="Aantal dozen"
+            v-model="this.tile.amountOfBoxes"
+          ></v-text-field>
+          <span class="mx-2">x</span>
+          <v-text-field
+            width="50%"
+            name="squareMetersPerBox"
+            label="m² per doos"
+            type="number"
+            v-model="this.tile.squareMetersPerBox"
+          ></v-text-field>
+        </div>
+
+        <div class="total-square-meters d-flex flex-row align-start w-100">
+          <div class="d-flex flex-column text-center justify-center align-center mr-5">
+            <v-checkbox-btn v-model="isTotalCalculated" @change="CalculateTotalSquareMeters()" hide-details></v-checkbox-btn>
+            <span style="color: #bbb;">Berekend</span>
+          </div>
+          <v-text-field
+            width="50%"
+            name="totalSquareMeters"
+            label="Totaal m²"
+            v-bind:disabled="isTotalCalculated"
+            v-model="this.tile.totalSquareMeters"
+          ></v-text-field>
+        </div>
       </div>
     </div>
 
     <div class="buttons w-90 mx-auto d-flex flex-row justify-space-between" z-index="2">
       <a @click="$router.back()"><v-btn width="42vw" color="red-darken-3" dark>Annuleren</v-btn></a>
-      <v-btn width="42vw" color="green-darken-2" dark :onclick="saveTile">Opslaan</v-btn>
+      <v-btn id="save-button" width="42vw" color="green-darken-2" dark :onclick="saveTile">
+        <v-btn-text>Opslaan</v-btn-text>
+      </v-btn>
     </div>
   </div>
 </template>
@@ -133,5 +138,21 @@ export default {
   position: absolute;
   bottom: 3em;
   left: 5vw;
+}
+
+@keyframes fadeOutRight {
+  from {
+    opacity: 1;
+  }
+
+  to {
+    opacity: 0;
+    transform: translate3d(20%, 0, 0);
+  }
+}
+
+.fadeOutRight {
+  animation-name: fadeOutRight;
+  animation-duration: 0.66s;
 }
 </style>
