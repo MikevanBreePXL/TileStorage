3eed53dbae4f71734cbf6dece670da1b2469ad5e
Mike van Bree <mike.vanbree@student.pxl.be>
2024-09-18 20:02:23 +0200

    Added slightly more margin space below top bar

--- Frontend/src/components/TopLogoBar.vue
+++ Frontend/src/components/TopLogoBar.vue
@@ -2,7 +2,7 @@
     <div class="top-bar d-flex justify-center w-vw bg-white">
         <RouterLink to="/entry"><img src="../assets/TegelsLogo.png" alt="Logo" class="logo my-5 flex justify-center"></RouterLink>
     </div>
-    <div class="top-bar-gradient mb-1">
+    <div class="top-bar-gradient mb-2">
     </div>
 </template>
 
